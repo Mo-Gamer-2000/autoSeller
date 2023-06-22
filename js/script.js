cd35b060f68f307848d28b04b0b9eee9cabffb96
Moeez Abdul <moeezrajput2000@gmail.com>
2023-06-22 12:06:49 +0100

    Working on Sliding Car Images on Homepage Func

--- js/script.js
+++ js/script.js
@@ -49,3 +49,43 @@ document.querySelector(".home").onmouseleave = (e) => {
     element.style.transform = `translateX(0px) translateX(0px)`;
   });
 };
+
+let wrapper = document.querySelector(".wrapper-box");
+let activeBox = wrapper.querySelectorAll(".box");
+let activeLabel = document
+  .querySelector(".activeCircle")
+  .querySelectorAll(".fa-circle");
+let nextBtn = document.querySelector("#nextBtn");
+let preBtn = document.querySelector("#preBtn");
+
+let indexNo = 0;
+
+nextBtn.onclick = () => {
+  indexNo++;
+  changeBox();
+};
+
+preBtn.onclick = () => {
+  indexNo--;
+  changeBox();
+};
+
+let changeBox = () => {
+  if (indexNo > activeBox.length - 1) {
+    indexNo = 0;
+  } else if (indexNo < 0) {
+    indexNo - activeBox.length - 1;
+  }
+
+  for (let i = 0; i < activeBox.length; i++) {
+    if (i === indexNo) {
+      activeBox[i].classList.add("active");
+      activeLabel[i].classList.add("fa-solid");
+
+      wrapper.style.transform = `translateX(${indexNo * -250}px)`;
+    } else {
+      activeBox[i].classList.remove("active");
+      activeLabel[i].classList.remove("fa-solid");
+    }
+  }
+};
